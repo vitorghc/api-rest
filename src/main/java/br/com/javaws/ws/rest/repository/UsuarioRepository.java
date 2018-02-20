bc8ca7bad9b98e3c73dc53663be5db62a47581f5
vitor <vitorghc@hotmail.com>
2018-02-19 22:11:52 -0300

    Todos os commits e diretórios

---
+++ src/main/java/br/com/javaws/ws/rest/repository/UsuarioRepository.java
@@ -0,0 +1,65 @@
+package br.com.javaws.ws.rest.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.javaws.ws.rest.entity.Usuario;
+
+public class UsuarioRepository {
+
+	private final EntityManagerFactory entityManagerFactory;
+
+	private final EntityManager entityManager;
+
+	public UsuarioRepository() {
+
+		this.entityManagerFactory = Persistence.createEntityManagerFactory("minha-persistence-unit");
+		this.entityManager = this.entityManagerFactory.createEntityManager();
+	}
+	
+	
+	//CRIANDO NOVO REGISTRO NO BANCO
+	public void salvar(Usuario usuario){
+		this.entityManager.getTransaction().begin();
+		this.entityManager.persist(usuario);
+		this.entityManager.getTransaction().commit();
+	}
+	
+	//ALTERANDO UM REGISTRO EXISTENTE
+	public void alterar(Usuario usuario){
+		 
+		this.entityManager.getTransaction().begin();
+		this.entityManager.merge(usuario);
+		this.entityManager.getTransaction().commit();
+	}
+	
+	
+	//RETORNA TODAS AS PESSOAS CADASTRADAS NO BANCO DE DADOS 
+	@SuppressWarnings("unchecked")
+	public List<Usuario> TodasPessoas(){
+ 
+		return this.entityManager.createQuery("SELECT u FROM Usuario u ORDER BY u.nome").getResultList();
+	}
+	
+	
+	
+	//CONSULTA UMA PESSOA CADASTRA PELO CÓDIGO
+	public Usuario GetPessoa(Integer codigo){
+ 
+		return this.entityManager.find(Usuario.class, codigo);
+	}
+	
+	public void excluir(Integer codigo) {
+		Usuario usuario = this.GetPessoa(codigo);
+
+		this.entityManager.getTransaction().begin();
+		this.entityManager.remove(usuario);
+		this.entityManager.getTransaction().commit();
+
+	}
+
+
+}
---
+++ src/main/java/br/com/javaws/ws/rest/service/ServiceUsuario.java
@@ -0,0 +1,94 @@
+package br.com.javaws.ws.rest.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import br.com.javaws.ws.rest.entity.Usuario;
+import br.com.javaws.ws.rest.http.UsuarioResponse;
+import br.com.javaws.ws.rest.repository.UsuarioRepository;
+
+@Path("/serviceUsuario")
+public class ServiceUsuario {
+
+	private final UsuarioRepository repository = new UsuarioRepository();
+
+	
+	/**
+	 * @param usuario
+	 * @return String
+	 */
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Path("/cadastrar")
+	public String cadastrar(UsuarioResponse usuario) {
+
+		Usuario entity = new Usuario();
+
+		try {
+
+			entity.setNome(usuario.getNome());
+			entity.setPassword(usuario.getPassword());
+
+			repository.salvar(entity);
+
+			return "Registro cadastrado com sucesso";
+		} catch (Exception e) {
+			return "Erro ao cadastrar um registro " + e.getMessage();
+		}
+	}
+	
+	
+	/**
+	 * Esse m�todo lista todas pessoas cadastradas na base
+	 * */
+	@GET
+	@Produces("application/json; charset=UTF-8")
+	@Path("/todasPessoas")
+	public List<UsuarioResponse> todasPessoas(){
+ 
+		List<UsuarioResponse> pessoas =  new ArrayList<UsuarioResponse>();
+ 
+		List<Usuario> listaEntityPessoas = repository.TodasPessoas();
+ 
+		for (Usuario entity : listaEntityPessoas) {
+ 
+			pessoas.add(new UsuarioResponse(entity.getCodigo(), entity.getNome(),entity.getPassword()));
+		}
+ 
+		return pessoas;
+	}
+	
+	
+	/**
+	 * Excluindo uma pessoa pelo código
+	 * */
+	@DELETE
+	@Produces("application/json; charset=UTF-8")
+	@Path("/excluir/{codigo}")	
+	public String excluir(@PathParam("codigo") Integer codigo){
+ 
+		try {
+ 
+			repository.excluir(codigo);
+ 
+			return "Registro excluido com sucesso!";
+ 
+		} catch (Exception e) {
+ 
+			return "Erro ao excluir o registro! " + e.getMessage();
+		}
+ 
+	}
+ 
+
+}
